38a746e43ad50bbbcc363a9d44bd71105cad60f5
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-09-11 22:51:53 +0530

    Update routes.py

--- AlumniConnect/app/alumni/routes.py
+++ AlumniConnect/app/alumni/routes.py
@@ -2,9 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from app.auth.routes import login_required, role_required
 from app.database.connection import fetch_all, fetch_one, execute, get_db
 
-
 alumni_bp = Blueprint('alumni', __name__, url_prefix='/alumni')
-
 
 @alumni_bp.route('/dashboard')
 @login_required
